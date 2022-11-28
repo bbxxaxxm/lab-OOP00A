1227ecd2d133075eb0eb603e04551535912afe3f
natnicha <natnicha.pro@kkumail.com>
2022-11-28 15:38:56 +0700

    add name,id,email

---
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Name
+Console.WriteLine("Natnicha prompik");
+//ID
+Console.WriteLine("653450284-3");
+//EMAIL
+Console.WriteLine("natniha.pro@kkumail.com");
